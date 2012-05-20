8d6dd5736cc8e2b72fb71fb6b283ff840c357e6c
y-yoshinoya <y2.void+github@gmail.com>
2012-05-21 00:32:05 +0900

    refactoring

--- src/main/scala/IOUtil.scala
+++ src/main/scala/IOUtil.scala
@@ -3,10 +3,37 @@ package com.github.aselab.activerecord
 import sbt._
 
 object IOUtil {
+
+  implicit def enumToIterator[A](e : java.util.Enumeration[A]) = new Iterator[A] {
+    def next = e.nextElement
+    def hasNext = e.hasMoreElements
+  }
+
   def baseTemplateDir(baseDirectory: File, templateDir: String): File =
     if (templateDir.startsWith("/"))
       file(templateDir)
     else
       baseDirectory / templateDir
+
+  def copyJarResourses(url: java.net.URL, destination: String, logger: Logger) {
+    url.openConnection match {
+      case connection: java.net.JarURLConnection =>
+        val entryName = connection.getEntryName
+        val jarFile = connection.getJarFile
+        jarFile.entries.filter(_.getName.startsWith(entryName)).foreach { e =>
+          val fileName = e.getName.drop(entryName.size)
+          val target = file(destination) / fileName
+          if (target.exists) {
+            logger.warn("already exists: " + target)
+          } else {
+            if (!e.isDirectory)
+              IO.transfer(jarFile.getInputStream(e), target)
+            else
+              IO.createDirectory(target)
+            logger.success("created: " + target)
+          }
+        }
+      }
+    }
 }
 
