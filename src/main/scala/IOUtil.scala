f577e44346737b4f727851f082ee7b6daa240c44
y-yoshinoya <y2.void+github@gmail.com>
2012-05-21 14:05:24 +0900

    add override check in console

--- src/main/scala/IOUtil.scala
+++ src/main/scala/IOUtil.scala
@@ -23,17 +23,30 @@ object IOUtil {
         jarFile.entries.filter(_.getName.startsWith(entryName)).foreach { e =>
           val fileName = e.getName.drop(entryName.size)
           val target = file(destination) / fileName
-          if (target.exists) {
-            logger.warn("already exists: " + target)
-          } else {
-            if (!e.isDirectory)
+          val message = if (target.exists) "overrided: " else "created: "
+          if (!e.isDirectory) {
+            if (safeToCreateFile(target)) {
               IO.transfer(jarFile.getInputStream(e), target)
-            else
-              IO.createDirectory(target)
-            logger.success("created: " + target)
+              logger.success(message + target)
+            }
+          } else {
+            IO.createDirectory(target)
+            if (!target.exists) logger.success("created: " + target)
           }
         }
       }
     }
+
+  def safeToCreateFile(file: File): Boolean = {
+    def askUser: Boolean = {
+      val question = "The file %s exists, do you want to override it? (y/n): ".format(file.getPath)
+      scala.Console.readLine(question).toLowerCase.headOption match {
+        case Some('y') => true
+        case Some('n') => false
+        case _ => askUser
+      }
+    }
+    if (file.exists) askUser else true
+  }
 }
 
